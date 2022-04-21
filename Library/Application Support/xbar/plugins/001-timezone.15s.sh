d0168a5bfc5d19a9e696f51b6c7db9f6d8964356
Florian Begusch <florian.begusch@gmail.com>
2022-04-21 13:23:21 +0000

    Add xbar plugins

---
+++ Library/Application Support/xbar/plugins/001-timezone.15s.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# <xbar.title>Timezone</xbar.title>
+# <xbar.version>v1.0.0</xbar.version>
+# <xbar.author>Toni Hoffmann</xbar.author>
+# <xbar.author.github>xremix</xbar.author.github>
+# <xbar.desc>Show the current time of a different timezone.</xbar.desc>
+
+#Prefix="AUT"
+Time_Zone="Europe/Vienna"
+#TZ=":$Time_Zone" date "+$Prefix %H:%M"
+TZ=":$Time_Zone" /usr/local/opt/coreutils/libexec/gnubin/date -Iminutes | sed 's#T#  #;s#+# +#'
