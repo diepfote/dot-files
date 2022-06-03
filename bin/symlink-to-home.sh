8d79399e372049ed84ba988e0beb029e62f59920
Florian Begusch <florian.begusch@gmail.com>
2022-06-03 07:04:23 +0000

    dot-files/bin/symlink-to-home.sh: ignore bin/ and Makefile

--- bin/symlink-to-home.sh
+++ bin/symlink-to-home.sh
@@ -3,12 +3,12 @@
 # ensure directories
 DOT_FILES_DIR=~/Documents/dot-files
 
-(cd "$DOT_FILES_DIR" && git ls-files | xargs -n 1 dirname | sed "s#^#'$HOME/##" | sed "s#\$#'##" | tr '\n' '\0' | xargs -n 1 -0 mkdir -p)
+(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile' | xargs -n 1 dirname | sed "s#^#'$HOME/##" | sed "s#\$#'##" | xargs -n 1 mkdir -p)
 
 # symlink all files
 while read -r line; do
   current_dir="$HOME/$(dirname "$line")"
   (cd "$current_dir" >/dev/null && \
      ln -fs "$(realpath --relative-to="$current_dir" "$DOT_FILES_DIR")/$line"   "$(basename "$line")")
-done < <(cd "$DOT_FILES_DIR" && git ls-files)
+done < <(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile')
 
