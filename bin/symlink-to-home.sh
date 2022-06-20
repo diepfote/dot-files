7ebb332bf83d15499a163d774fdce47248d208b9
Florian Begusch <florian.begusch@gmail.com>
2022-06-20 20:31:46 +0200

    formatting

--- bin/symlink-to-home.sh
+++ bin/symlink-to-home.sh
@@ -3,7 +3,11 @@
 # ensure directories
 DOT_FILES_DIR="$(realpath "$(git rev-parse --show-toplevel)")"
 
-(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile' | xargs -n 1 dirname | sed "s#^#'$HOME/##" | sed "s#\$#'##" | xargs -n 1 mkdir -p)
+(cd "$DOT_FILES_DIR" && git ls-files |\
+                          grep -vE '^bin/|Makefile' |\
+                          xargs -n 1 dirname |\
+                          sed "s#^#'$HOME/##" |\ sed "s#\$#'##" |\
+                          xargs -n 1 mkdir -p)
 
 # symlink all files
 while read -r line; do
