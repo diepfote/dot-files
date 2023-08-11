4cae2aca255c9a84b12c7d44dc00b800174eb3a4
Florian Sorko <florian.begusch@gmail.com>
2023-08-11 06:22:47 +0000

    Update linking
    A symlinked main config file disables/breaks karabiner-elements
    keybindings.
    We know copy the config to the dot-files repo to update the repo's
    state. Or if it does not exist in the config location, we copy it from
    the repo to the config location

--- bin/symlink-to-home.sh
+++ bin/symlink-to-home.sh
@@ -10,16 +10,36 @@ DOT_FILES_DIR="$(realpath "$(git rev-parse --show-toplevel)")"
                           sed "s#\$#'##" |\
                           xargs -n 1 mkdir -p)
 
+system="$(uname)"
 # symlink all files
 while read -r line; do
-  if [[ "$(uname)" != Darwin && "$line" =~ .*xbar.* ]]; then
-    # skip xbar.app plugins for Linux. Darwin only
+  if [[ "$system" != Darwin && "$line" =~ .*xbar.* ]]; then
+    # skip xbar.app plugins for Linux. Mac OS only
+    continue
+  fi
+  if [[ "$line" =~ .*\.config/karabiner/karabiner\.json ]]; then
+
+    if [ "$system" = Darwin ]; then
+      if [ ! -f ~/"$line" ]; then
+        # do not symlink as this breaks keybindings
+        # and do not link if this is linux -> so both continue
+        cp "$line" ~/"$line"
+      else
+        set -x
+        # in case the file exists copy it to the dot-files repo -> to commit changes
+        cp ~/"$line" "$line"
+        set +x
+      fi
+    fi
+
+    # do not override custom behavior with a symlink
     continue
   fi
 
   current_dir="$HOME/$(dirname "$line")"
   (cd "$current_dir" >/dev/null && \
      ln -fs "$(realpath --relative-to="$current_dir" "$DOT_FILES_DIR")/$line"   "$(basename "$line")")
+
 done < <(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile')
 
 
