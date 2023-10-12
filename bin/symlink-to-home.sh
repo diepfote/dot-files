ec0cb4aa7cd1b810e60f6ca55a26b892ce8b80de
Florian Sorko <florian.begusch@gmail.com>
2023-10-12 05:48:16 +0000

    cleanup

--- bin/symlink-to-home.sh
+++ bin/symlink-to-home.sh
@@ -1,5 +1,43 @@
 #!/usr/bin/env bash
 
+_skip_exclusions () {
+  if [ "$system" != Darwin ]; then
+    # skip Mac OS only config on Linux
+    if \
+       [[ "$line" =~ .*xbar.* ]] || \
+       [[ "$line" =~ .*karabiner.* ]] || \
+       [[ "$line" =~ .*lima.* ]] || \
+       [[ "$line" =~ .*Library/Application.* ]]; then
+      return 0
+    fi
+  else
+    # skip Linux only config
+    if \
+       [[ "$line" =~ .*\.config/autostart.* ]] || \
+       [[ "$line" =~ .*\.config/i3.* ]]; then
+      return 0
+    fi
+  fi
+
+  return 1
+}
+
+_ensure_dir_exists () {
+
+(
+  die () { echo Failed to cd dir; exit 1; }
+  cd "$DOT_FILES_DIR" || die
+
+  _dir="$(dirname "$1" | sed "s#^#$HOME/##")"
+
+  if [ "$_dir" = "$HOME"/. ]; then
+    exit
+  fi
+  echo "[.] ensure exists $_dir"
+  mkdir -p "$_dir"
+)
+
+}
 
 _copy-file-to-dot-files () {
   cp ~/"$1" "$1"
@@ -8,27 +46,34 @@ _copy-file-to-dot-files () {
 # ensure directories
 DOT_FILES_DIR="$(realpath "$(git rev-parse --show-toplevel)")"
 
-(cd "$DOT_FILES_DIR" && git ls-files |\
-                          grep -vE '^bin/|Makefile' |\
-                          xargs -n 1 dirname |\
-                          sed "s#^#'$HOME/##" |\
-                          sed "s#\$#'##" |\
-                          xargs -n 1 mkdir -p)
 
 system="$(uname)"
-# symlink all files
+
+# ensure dirs exist
 while read -r line; do
-  if [[ "$system" != Darwin && "$line" =~ .*xbar.* ]]; then
-    # skip xbar.app plugins for Linux. Mac OS only
+
+  if _skip_exclusions; then
     continue
   fi
-  echo $line
+
+  _ensure_dir_exists "$line"
+done < <(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile')
+
+
+# symlink all files
+while read -r line; do
+
+  if _skip_exclusions; then
+    continue
+  fi
+
   if [[ "$line" =~ .*\.config/karabiner/karabiner\.json ]]; then
       # do not symlink as this breaks keybindings
       # and do not link if this is linux -> so both continue
 
     if [ "$system" = Darwin ]; then
       if [ ! -f ~/"$line" ]; then
+        echo "$line"
         cp "$line" ~/"$line"
       else
         set -x
@@ -38,11 +83,14 @@ while read -r line; do
       fi
     fi
 
+
     # do not override custom behavior with a symlink
     # and ignore on Linux
     continue
   fi
 
+
+  echo "$line"
   current_dir="$HOME/$(dirname "$line")"
   (cd "$current_dir" >/dev/null && \
      ln -fs "$(realpath --relative-to="$current_dir" "$DOT_FILES_DIR")/$line"   "$(basename "$line")")
@@ -50,6 +98,7 @@ while read -r line; do
 done < <(cd "$DOT_FILES_DIR" && git ls-files | grep -vE '^bin/|Makefile')
 
 
+# LIMA specific
 # link tool folders if in container or in lima vm/lima-vm
 if [ -n "$NOT_HOST_ENV" ]; then
   # Hint all tool folders sit right next to $DOT_FILES_DIR
